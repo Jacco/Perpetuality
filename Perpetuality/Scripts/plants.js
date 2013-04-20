ac1aad5bbcfc998edc24438b0bc9d476d13b2d07
Casper de Geus <casper@webenable.nl>
2013-04-20 16:29:24 +0200

    First prototype of Plant placing UI

---
+++ Perpetuality/Scripts/plants.js
@@ -0,0 +1,26 @@
+﻿var perpetuality = perpetuality || {};
+
+perpetuality.plant = function () {
+    var number = 0;
+
+    function removePlant(plantId) {
+        $("#" + plantId).remove();
+    }
+
+    function placePlant(x, y) {
+        var plantId = "plant" + number++;
+        var newPlant = $("<div />", {
+            id: plantId,
+            "class": "plant",
+            style: "top: " + y + "px; " + "left: " + x + "px;"
+        });
+        newPlant.click(function (e) { removePlant(plantId) });
+        newPlant.appendTo("body");
+    }
+
+    var plant = $("#plantsource");
+    plant.draggable({ revert: true });
+    plant.on({ mouseup: function (e) { placePlant(e.pageX, e.pageY) } });
+
+    return plant;
+}();
