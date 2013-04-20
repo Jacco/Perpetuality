b6c443a768aa4a7bc326e47393b4828aa4c90201
Eric Platon <zaraki@gmx.com>
2013-04-20 17:21:51 +0900

    Embed the JS code in a namespace.
    This should facilitate the integration in the main codebase.

--- Perpetuality/Views/Map/perpetuality_map.js
+++ Perpetuality/Views/Map/perpetuality_map.js
@@ -4,22 +4,19 @@
  *      left for detailed info on selected power plant,
  *      right for dragging new plants on the map.
  */
-function initialize() {
+var perpetuality = perpetuality || {};
+
+perpetuality.map = perpetuality.map || function() {};
+
+perpetuality.map.prototype.init = function() {
   var mapOptions = {
     center: new google.maps.LatLng(42.34941, -71.056137),
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
-//google.maps.event.addDomListener(window, 'load', initialize);
+};
 
-function loadScript() {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDxynbvhnTHYl8nIIAoct1AjbJCxWZSwPw&sensor=false&callback=initialize";
-  document.body.appendChild(script);
-}
-
-window.onload = loadScript;
+// Keep at the end. Until the code is integrated with jQuery.
+window.onload = function() { (new perpetuality.map).init(); };
 
