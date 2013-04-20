bd62d6c85215b7a85ca39e0471f39874e80cacde
Jacco Kulman <jacco@jaap.nl>
2013-04-20 15:42:43 +0200

    Added initial email functionality

---
+++ Perpetuality/App_Code/Model_EmailConfirmation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Perpetuality.App_Code
+{
+    public class Model_EmailConfirmation
+    {
+        public Model_EmailConfirmation(string id)
+        {
+
+        }
+    }
+}
---
+++ Perpetuality/Controllers/MailController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Perpetuality.Controllers
+{
+    public class MailController : Controller
+    {
+        public ActionResult Index(string view, string id)
+        {
+            return View(view, (object)id);
+        }
+    }
+}
