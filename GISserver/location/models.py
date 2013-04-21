f389dbd90093a0d11f654cd28150c2fa8cf9d7a8
m.vriens <marijnvriens@gmail.com>
2013-04-21 13:30:19 +0200

    Fixed windspeed.

--- GISserver/location/models.py
+++ GISserver/location/models.py
@@ -2,12 +2,20 @@ from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
 from django.db import connection
 
-class Urban(models.Model):
-    gid = models.IntegerField(primary_key=True)
-    scalerank = models.DecimalField(max_digits=10, decimal_places=0)
-    featurecla = models.TextField()
-    area_sqkm = models.FloatField()
-    geom =  models.MultiPolygonField()
+#class Urban(models.Model):
+#    gid = models.IntegerField(primary_key=True)
+#    scalerank = models.DecimalField(max_digits=10, decimal_places=0)
+#    featurecla = models.TextField()
+#    area_sqkm = models.FloatField()
+#    geom =  models.MultiPolygonField()
+#
+#    @classmethod
+#    def get_point(cls, long, lat):
+#        p = Point(long, lat)
+#        return cls.objects.filter(geom__contains=Point).get()
+#
+#    class Meta:
+#        db_table = 'urban'
 
 class Solar(models.Model):
     value = models.FloatField() # watts per m^2.
@@ -22,14 +30,15 @@ class Solar(models.Model):
         return solar
 
 class WindSpeed(models.Model):
-    gid = models.IntegerField()
-    geom = models.GeometryField()
+    gid = models.IntegerField(primary_key=True)
+#    geom = models.GeometryField()
     total = models.FloatField()
 
     @classmethod
     def get_point(cls, long, lat):
-        p = Point(long, lat)
-        return cls.objects.filter(geom__contains=Point).get()
-
+        return list(cls.objects.raw("""SELECT gid, total
+            FROM windspeed
+            ORDER BY windspeed.geom <-> ST_Point(%s,%s)
+            LIMIT 1""", [long, lat]))[0].total
     class Meta:
         db_table = 'windspeed'
