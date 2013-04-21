afd68e92b4a386d0e1456e5fddebfa2f4fe03efe
Jacco Kulman <jacco@jaap.nl>
2013-04-21 05:54:41 +0200

    Integrated Eric's HeatMap code.

--- Perpetuality/Scripts/perpetuality_map.js
+++ Perpetuality/Scripts/perpetuality_map.js
@@ -3,22 +3,120 @@
 var perpetuality = perpetuality || {};
 
 /**
- * Map
+ * Map for Perpetuality.
+ *
+ * Support for heatmaps if a server can deliver arrays of objects as:
+ *   {
+ *   "lat": 35.123,
+ *   "lon": 139.123,
+ *   "weight": 123
+ *   }
+ * The client here attempts to connect to /api/dataset---easy to change.
+ * Query parameters are a pair of ranges:
+ *   {
+ *   "lat_r": [ 35.1, 36.2 ],
+ *   "lon_r": [ 135.1, 136.2 ],
+ *   }
  */
-perpetuality.map = perpetuality.map || function() {
+perpetuality.map = perpetuality.map || function () {
   this.root = {};
+  this.dataServer = window.location.protocol + "//" + window.location.host;
   this.panes = {};
-  this.overlays = [];
-  this.sprites = [];
+  this.heatMapCache = {};
+};
+
+perpetuality.map.prototype.buildMap = function(options) {
+  return new google.maps.Map(document.getElementById("map"), options || {});
 };
 
 perpetuality.map.prototype.init = function() {
   var mapOptions = {
-    center: new google.maps.LatLng(42.34941, -71.056137),
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: new google.maps.LatLng(37.0625, -95.677068), // Boston
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+    },
+    streetViewControl: false
   };
-  this.root = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var map = this.buildMap(mapOptions);
+
+  var me = this;
+  var updateMap = function() {
+    var targetURL = me.dataServer + "/api/dataset";
+    var northEast = this.getBounds().getNorthEast();
+    var southWest = this.getBounds().getSouthWest();
+    var data = {
+      lon_r: [ southWest.lng(), northEast.lng() ],
+      lat_r: [ southWest.lat(), northEast.lat() ],
+    };
+    if (me.dataServer != (window.location.protocol + "//" + window.location.host)) {
+      $.ajax(targetURL, {
+        data: data,
+        dataType: "jsonp",
+        success: function(data) {
+          me.buildHeatMapLayer(data);
+        }
+      });
+    } else {
+      $.ajax(targetURL, {
+        data: data,
+        dataType: "json",
+        timeout: 5,
+        success: function(data, textStatus, jqXHR) {
+          me.buildHeatMapLayer(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          alert("failed to update heat map data!");
+        }
+      });
+    }
+  };
+  google.maps.event.addListenerOnce(map, 'center_changed', updateMap);
+  google.maps.event.addListener(map, 'dragend', updateMap);
+  google.maps.event.addListener(map, 'zoom_changed', updateMap);
+
+  this.root = map;
+};
+
+/**
+ * Layers.
+ */
+perpetuality.map.prototype.buildHeatMapLayer = function (data) {
+    var me = this;
+    if (this.heatMap) {
+        me.heatMap.setMap(null);
+    }
+    var zoom = "" + me.root.getZoom();
+    var center = "(" + me.root.getCenter().lat() + "," + me.root.getCenter().lng() + ")";
+    var dataSet = [];
+    if (me.heatMapCache[zoom] == undefined || me.heatMapCache[zoom][center] == undefined) {
+        $(data).each(function (idx, d) {
+            dataSet.push({
+                location: new google.maps.LatLng(d.lat, d.lon),
+                weight: d.weight
+            });
+        });
+    }
+
+    if (me.heatMapCache[zoom] == undefined) {
+        me.heatMapCache[zoom] = {};
+    }
+
+    if (me.heatMapCache[zoom][center] == undefined) {
+        var heatmap = undefined;
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: dataSet
+        });
+        heatmap.setOptions({
+            radius: $("#map").width() / 20
+        });
+        me.heatMapCache[zoom][center] = heatmap;
+    } else {
+        heatmap = me.heatMapCache[zoom][center];
+    }
+    heatmap.setMap(this.root);
+    me.heatMap = heatmap;
 };
 
 /**
@@ -142,12 +240,31 @@ $(document).ready(function () {
 
   var map = new perpetuality.map;
   map.init();
+//  map.dataServer = "http://localhost:3000" Change to proper address.
+  /**
+   * Use current location if available.
+   */
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var center = new google.maps.LatLng(
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      map.root.setCenter(center);
+    }, function() {});
+  } else {
+    // TODO Need to generate the layers?
+  }
+
+
     /**
      * Panes.
      */
   var overlayControlPane = map.makeItemizedPane("overlay", [{
       "title": "HeatMap",
-      "action": function () { alert("HeatMap") },
+      action: function() {
+        map.heatMap.setMap(map.heatMap.getMap() ? null : map.root);
+      },
       "image": "/Content/Images/default-image.jpeg",
       "itemExtraClass": "map-pane-item-horizontal"
   }], "map-pane-bottom");
