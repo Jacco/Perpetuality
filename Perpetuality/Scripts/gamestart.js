c722e8610d295ea4e4087646cde6fde9320c0fce
Bart Enkelaar <b.enkelaar@topdesk.com>
2013-04-23 19:49:33 +0200

    Removed class duplication from animation code

--- Perpetuality/Scripts/gamestart.js
+++ Perpetuality/Scripts/gamestart.js
@@ -3,29 +3,29 @@
 perpetuality.start = perpetuality.start || {};
 
 perpetuality.start.animatedBegin = function () {
-    function recursiveAnimate(lever, animationArray, callback) {
+    function recursiveAnimate(lever, from, animationArray, callback) {
         if (animationArray.length < 1) {
             callback();
         }
         else {
             var animation = animationArray.shift();
             lever.animate({"font-size": "+=20"}, animation.in, "linear", function () {
-                lever.removeClass(animation.from);
+                lever.removeClass(from);
                 lever.addClass(animation.to);
-                recursiveAnimate(lever, animationArray, callback);
+                recursiveAnimate(lever, animation.to, animationArray, callback);
             });
         }
     };
 
     var startGameCallback = function() { $("#starter").submit(); };
-    recursiveAnimate($(".startgame"), [
-        { from: "leverup",          to: "levercrackle1","in": 80 },
-        { from: "levercrackle1",    to: "levercrackle2","in": 80 },
-        { from: "levercrackle2",    to: "levercrackle1","in": 150 },
-        { from: "levercrackle1",    to: "levercrackle2","in": 150 },
-        { from: "levercrackle2",    to: "leverup",      "in": 220 },
-        { from: "leverup",          to: "leverdown",    "in": 300 },
-        { from: "leverdown",        to: "leverdown",    "in": 300 }
+    recursiveAnimate($(".startgame"), "leverup", [
+        { to: "levercrackle1", milis: 80 },
+        { to: "levercrackle2", milis: 80 },
+        { to: "levercrackle1", milis: 150 },
+        { to: "levercrackle2", milis: 150 },
+        { to: "leverup",       milis: 220 },
+        { to: "leverdown",     milis: 300 },
+        { to: "leverdown",     milis: 300 }
     ], startGameCallback);
 }
 
